607b0a223c52b1daada94b4e32743bc6d6588cb9
hederalves <alvesheder92@gmail.com>
2024-07-03 23:20:11 -0300

    feat: create the global styles and application theme

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+    colors: {
+        primary: '#4b0082',
+        secondary: '#2d003e',
+        background: '#f0f2f5',
+        white: '#fff',
+        black: '#000',
+    },
+};
