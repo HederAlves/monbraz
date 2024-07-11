8931433557ec85adde43ff1119087c2b544cbaf9
hederalves <alvesheder92@gmail.com>
2024-07-11 00:09:02 -0300

    feat: create the list in the manage components and improve the design

--- src/components/accessories/ManageAccessories.tsx
+++ src/components/accessories/ManageAccessories.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc } from 'firebase/firestore';
 import firestore from '../../firebaseConfig';
+import styled from 'styled-components';
+import { FaTrash, FaEdit } from 'react-icons/fa';
 
 interface Accessories {
     id: string;
@@ -11,9 +13,120 @@ interface Accessories {
     situation: string;
 }
 
+const Container = styled.div`
+    width: 800px;
+    padding: 20px 20px 5px 20px;
+    background-color: #f5f5f5;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const TitleContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+`;
+
+const FormContainer = styled.div`
+    margin-bottom: 20px;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
+
+const FormRow = styled.div`
+    display: flex;
+    gap: 10px;
+`;
+
+const TableContainer = styled.div`
+    width: 800px;
+    margin-top: 16px;
+    background-color: #f5f5f5;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    overflow-x: auto;
+`;
+
+const Table = styled.table`
+    width: 100%;
+    border-collapse: collapse;
+`;
+
+const TableHeader = styled.thead`
+    background-color: ${(props) => props.theme.colors.secondary};
+    color: white;
+`;
+
+const TableRow = styled.tr`
+    &:nth-child(even) {
+        background-color: #f5f5f5;
+    }
+`;
+
+const TableCell = styled.td`
+    border-bottom: 1px solid #ccc;
+    text-align: center;
+    &:first-child {
+        text-align: left;
+    }
+`;
+
+const TableCellHeader = styled.th`
+    padding: 10px;
+    border: 1px solid #ccc;
+    text-align: center;
+    &:first-child {
+        text-align: left;
+    }
+`;
+
+const Input = styled.input`
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 16px;
+    width: 100%;
+    box-sizing: border-box;
+    &:focus {
+        border-color: #3f51b5;
+        box-shadow: 0 0 5px rgba(63, 81, 181, 0.5);
+        outline: none;
+    }
+`;
+
+const Button = styled.button`
+    margin-top: 8px;
+    background-color: ${(props) => props.theme.colors.primary};
+    color: white;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+    &:hover {
+        background-color: ${(props) => props.theme.colors.secondary};
+    }
+`;
+
+const CancelButton = styled.button`
+    background-color: #ffffff;
+    color: #f44336;
+    &:hover {
+        background-color: #ffffff;
+        color: #800000;
+        border-color: #800000;
+    }
+`;
+
 const ManageAccessories: React.FC = () => {
     const [accessories, setAccessories] = useState<Accessories[]>([]);
     const [form, setForm] = useState<Partial<Accessories>>({});
+    const [isEditing, setIsEditing] = useState(false);
 
     useEffect(() => {
         const fetchAccessories = async () => {
@@ -38,11 +151,10 @@ const ManageAccessories: React.FC = () => {
         e.preventDefault();
 
         if (form.id) {
-            // Atualizar acessório existente
             const accessoryRef = doc(firestore, 'accessories', form.id);
             await updateDoc(accessoryRef, form);
+            setIsEditing(false);
         } else {
-            // Adicionar novo acessório
             await addDoc(collection(firestore, 'accessories'), form);
         }
 
@@ -52,11 +164,17 @@ const ManageAccessories: React.FC = () => {
 
     const handleEdit = (accessory: Accessories) => {
         setForm(accessory);
+        setIsEditing(true);
     };
 
     const handleDelete = async (id: string) => {
         await deleteDoc(doc(firestore, 'accessories', id));
         fetchAccessories();
+    };
+
+    const handleCancelEdit = () => {
+        setForm({});
+        setIsEditing(false);
     };
 
     const fetchAccessories = async () => {
@@ -66,62 +184,89 @@ const ManageAccessories: React.FC = () => {
     };
 
     return (
-        <div>
-            <h2>Gerenciar Acessórios</h2>
-
-            <form onSubmit={handleFormSubmit}>
-                <input
-                    type="text"
-                    name="code"
-                    placeholder="Código"
-                    value={form.code || ''}
-                    onChange={handleInputChange}
-                />
-                <input
-                    type="date"
-                    name="dataPurchase"
-                    placeholder="Data da Compra"
-                    value={form.dataPurchase || ''}
-                    onChange={handleInputChange}
-                />
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Nome"
-                    value={form.name || ''}
-                    onChange={handleInputChange}
-                />
-                <input
-                    type="text"
-                    name="observation"
-                    placeholder="Observação"
-                    value={form.observation || ''}
-                    onChange={handleInputChange}
-                />
-                <input
-                    type="text"
-                    name="situation"
-                    placeholder="Situação"
-                    value={form.situation || ''}
-                    onChange={handleInputChange}
-                />
-                <button type="submit">{form.id ? 'Atualizar' : 'Adicionar'}</button>
-            </form>
-
-            <ul>
-                {accessories.map(accessory => (
-                    <li key={accessory.id}>
-                        <strong>Código:</strong> {accessory.code} <br />
-                        <strong>Data da Compra:</strong> {accessory.dataPurchase} <br />
-                        <strong>Nome:</strong> {accessory.name} <br />
-                        <strong>Observação:</strong> {accessory.observation} <br />
-                        <strong>Situação:</strong> {accessory.situation} <br />
-                        <button onClick={() => handleEdit(accessory)}>Editar</button>
-                        <button onClick={() => handleDelete(accessory.id)}>Excluir</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <>
+            <Container>
+                <TitleContainer>
+                    <h3>{isEditing ? 'Atualizar Acessório' : 'Adicionar Acessório'}</h3>
+                    {isEditing && <CancelButton onClick={handleCancelEdit}>Cancelar Atualização</CancelButton>}
+                </TitleContainer>
+                <FormContainer>
+                    <Form onSubmit={handleFormSubmit}>
+                        <FormRow>
+                            <Input
+                                type="text"
+                                name="code"
+                                placeholder="Código"
+                                value={form.code || ''}
+                                onChange={handleInputChange}
+                            />
+                            <Input
+                                type="date"
+                                name="dataPurchase"
+                                placeholder="Data da Compra"
+                                value={form.dataPurchase || ''}
+                                onChange={handleInputChange}
+                            />
+                        </FormRow>
+                        <FormRow>
+                            <Input
+                                type="text"
+                                name="name"
+                                placeholder="Nome"
+                                value={form.name || ''}
+                                onChange={handleInputChange}
+                            />
+                            <Input
+                                type="text"
+                                name="observation"
+                                placeholder="Observação"
+                                value={form.observation || ''}
+                                onChange={handleInputChange}
+                            />
+                        </FormRow>
+                        <FormRow>
+                            <Input
+                                type="text"
+                                name="situation"
+                                placeholder="Situação"
+                                value={form.situation || ''}
+                                onChange={handleInputChange}
+                            />
+                        </FormRow>
+                        <Button type="submit">{isEditing ? 'Atualizar' : 'Adicionar'}</Button>
+                    </Form>
+                </FormContainer>
+            </Container>
+            <TableContainer>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableCellHeader>Código</TableCellHeader>
+                            <TableCellHeader>Data da Compra</TableCellHeader>
+                            <TableCellHeader>Nome</TableCellHeader>
+                            <TableCellHeader>Observação</TableCellHeader>
+                            <TableCellHeader>Situação</TableCellHeader>
+                            <TableCellHeader>Ações</TableCellHeader>
+                        </TableRow>
+                    </TableHeader>
+                    <tbody>
+                        {accessories.map(accessory => (
+                            <TableRow key={accessory.id}>
+                                <TableCell style={{ paddingLeft: '10px' }}>{accessory.code}</TableCell>
+                                <TableCell>{accessory.dataPurchase}</TableCell>
+                                <TableCell>{accessory.name}</TableCell>
+                                <TableCell>{accessory.observation}</TableCell>
+                                <TableCell>{accessory.situation}</TableCell>
+                                <TableCell>
+                                    <button onClick={() => handleEdit(accessory)}><FaEdit /></button>
+                                    <button onClick={() => handleDelete(accessory.id)}><FaTrash /></button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </tbody>
+                </Table>
+            </TableContainer>
+        </>
     );
 };
 
--- src/components/employees/ManageEmployees.tsx
+++ src/components/employees/ManageEmployees.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect, FormEvent } from 'react';
 import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc } from 'firebase/firestore';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import firestore from '../../firebaseConfig';
+import styled from 'styled-components';
+import { FaTrash, FaEdit } from 'react-icons/fa';
 
 interface Employee {
     id: string;
@@ -13,24 +15,140 @@ interface Employee {
     imageUrl?: string;
 }
 
+const Container = styled.div`
+    width: 800px;
+    padding: 20px 20px 5px 20px;
+    background-color: #f5f5f5;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const TitleContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+`;
+
+const FormContainer = styled.div`
+    margin-bottom: 20px;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
+
+const FormRow = styled.div`
+    display: flex;
+    gap: 10px;
+`;
+
+const TableContainer = styled.div`
+    width: 800px;
+    margin-top: 16px;
+    background-color: #f5f5f5;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    overflow-x: auto;
+`;
+
+const Table = styled.table`
+    width: 100%;
+    border-collapse: collapse;
+`;
+
+const TableHeader = styled.thead`
+    background-color: ${(props) => props.theme.colors.secondary};
+    color: white;
+`;
+
+const TableRow = styled.tr`
+    &:nth-child(even) {
+        background-color: #f5f5f5;
+    }
+`;
+
+const TableCell = styled.td`
+    border-bottom: 1px solid #ccc;
+    text-align: center;
+
+    &:first-child {
+        text-align: left;
+    }
+`;
+
+const TableCellHeader = styled.th`
+    padding: 10px;
+    border: 1px solid #ccc;
+    text-align: center;
+
+    &:first-child {
+        text-align: left;
+    }
+`;
+
+const Input = styled.input`
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 16px;
+    width: 100%;
+    box-sizing: border-box;
+
+    &:focus {
+        border-color: #3f51b5;
+        box-shadow: 0 0 5px rgba(63, 81, 181, 0.5);
+        outline: none;
+    }
+`;
+
+const Button = styled.button`
+    margin-top: 8px;
+    background-color: ${(props) => props.theme.colors.primary};
+    color: white;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+
+    &:hover {
+        background-color: ${(props) => props.theme.colors.secondary};
+    }
+`;
+
+const CancelButton = styled.button`
+    background-color: #ffffff;
+    color: #f44336;
+
+    &:hover {
+        background-color: #ffffff;
+        color: #800000;
+        border-color: #800000;
+    }
+`;
+
 const ManageEmployees: React.FC = () => {
     const [employees, setEmployees] = useState<Employee[]>([]);
     const [form, setForm] = useState<Partial<Employee>>({});
     const [imageFile, setImageFile] = useState<File | null>(null);
+    const [isEditing, setIsEditing] = useState(false);
 
     useEffect(() => {
-        const fetchEmployees = async () => {
-            try {
-                const employeeSnapshot = await getDocs(collection(firestore, 'employees'));
-                const employeesData: Employee[] = employeeSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })) as Employee[];
-                setEmployees(employeesData);
-            } catch (error) {
-                console.error('Error fetching employees: ', error);
-            }
-        };
-
         fetchEmployees();
     }, []);
+
+    const fetchEmployees = async () => {
+        try {
+            const employeeSnapshot = await getDocs(collection(firestore, 'employees'));
+            const employeesData: Employee[] = employeeSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })) as Employee[];
+            setEmployees(employeesData);
+        } catch (error) {
+            console.error('Error fetching employees: ', error);
+        }
+    };
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
@@ -46,19 +164,24 @@ const ManageEmployees: React.FC = () => {
     const handleFormSubmit = async (e: FormEvent) => {
         e.preventDefault();
 
-        let imageUrl = form.imageUrl;
+        let imageUrl = '';
         if (imageFile) {
             const storage = getStorage();
             const storageRef = ref(storage, `employees/${imageFile.name}`);
             await uploadBytes(storageRef, imageFile);
             imageUrl = await getDownloadURL(storageRef);
+        } else {
+            imageUrl = '';
         }
+
+        const newEmployee = { ...form, imageUrl };
 
         if (form.id) {
             const employeeRef = doc(firestore, 'employees', form.id);
-            await updateDoc(employeeRef, { ...form, imageUrl });
+            await updateDoc(employeeRef, newEmployee);
+            setIsEditing(false);
         } else {
-            await addDoc(collection(firestore, 'employees'), { ...form, imageUrl });
+            await addDoc(collection(firestore, 'employees'), newEmployee);
         }
 
         setForm({});
@@ -68,6 +191,7 @@ const ManageEmployees: React.FC = () => {
 
     const handleEdit = (employee: Employee) => {
         setForm(employee);
+        setIsEditing(true);
     };
 
     const handleDelete = async (id: string) => {
@@ -75,76 +199,101 @@ const ManageEmployees: React.FC = () => {
         fetchEmployees();
     };
 
-    const fetchEmployees = async () => {
-        const employeeSnapshot = await getDocs(collection(firestore, 'employees'));
-        const employeesData: Employee[] = employeeSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })) as Employee[];
-        setEmployees(employeesData);
+    const handleCancelEdit = () => {
+        setForm({});
+        setIsEditing(false);
     };
 
     return (
-        <div>
-            <h2>Gerenciar Funcionários</h2>
-
-            <form onSubmit={handleFormSubmit}>
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Nome"
-                    value={form.name || ''}
-                    onChange={handleInputChange}
-                />
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={form.email || ''}
-                    onChange={handleInputChange}
-                />
-                <input
-                    type="text"
-                    name="office"
-                    placeholder="Cargo"
-                    value={form.office || ''}
-                    onChange={handleInputChange}
-                />
-                <input
-                    type="text"
-                    name="permissions"
-                    placeholder="Permissões"
-                    value={form.permissions || ''}
-                    onChange={handleInputChange}
-                />
-                <input
-                    type="text"
-                    name="phone"
-                    placeholder="Telefone"
-                    value={form.phone || ''}
-                    onChange={handleInputChange}
-                />
-                <input
-                    type="file"
-                    name="image"
-                    placeholder="Imagem"
-                    onChange={handleFileChange}
-                />
-                <button type="submit">{form.id ? 'Atualizar' : 'Adicionar'}</button>
-            </form>
-
-            <ul>
-                {employees.map(employee => (
-                    <li key={employee.id}>
-                        {employee.imageUrl && <img src={employee.imageUrl} alt={employee.name} width="50" height="50" style={{ borderRadius: '50%' }} />}
-                        <strong>Nome:</strong> {employee.name} <br />
-                        <strong>Email:</strong> {employee.email} <br />
-                        <strong>Cargo:</strong> {employee.office} <br />
-                        <strong>Permissões:</strong> {employee.permissions} <br />
-                        <strong>Telefone:</strong> {employee.phone} <br />
-                        <button onClick={() => handleEdit(employee)}>Editar</button>
-                        <button onClick={() => handleDelete(employee.id)}>Excluir</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <>
+            <Container>
+                <TitleContainer>
+                    <h3>{isEditing ? 'Atualizar Funcionário' : 'Adicionar Funcionário'}</h3>
+                    {isEditing && <CancelButton onClick={handleCancelEdit}>Cancelar Atualização</CancelButton>}
+                </TitleContainer>
+                <FormContainer>
+                    <Form onSubmit={handleFormSubmit}>
+                        <FormRow>
+                            <Input
+                                type="text"
+                                name="name"
+                                placeholder="Nome"
+                                value={form.name || ''}
+                                onChange={handleInputChange}
+                            />
+                            <Input
+                                type="email"
+                                name="email"
+                                placeholder="Email"
+                                value={form.email || ''}
+                                onChange={handleInputChange}
+                            />
+                        </FormRow>
+                        <FormRow>
+                            <Input
+                                type="text"
+                                name="office"
+                                placeholder="Cargo"
+                                value={form.office || ''}
+                                onChange={handleInputChange}
+                            />
+                            <Input
+                                type="text"
+                                name="permissions"
+                                placeholder="Permissões"
+                                value={form.permissions || ''}
+                                onChange={handleInputChange}
+                            />
+                        </FormRow>
+                        <FormRow>
+                            <Input
+                                type="text"
+                                name="phone"
+                                placeholder="Telefone"
+                                value={form.phone || ''}
+                                onChange={handleInputChange}
+                            />
+                            <Input
+                                type="file"
+                                name="image"
+                                placeholder="Imagem"
+                                onChange={handleFileChange}
+                            />
+                        </FormRow>
+                        <Button type="submit">{isEditing ? 'Atualizar' : 'Adicionar'}</Button>
+                    </Form>
+                </FormContainer>
+            </Container>
+            <TableContainer>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableCellHeader>Nome</TableCellHeader>
+                            <TableCellHeader>Email</TableCellHeader>
+                            <TableCellHeader>Cargo</TableCellHeader>
+                            <TableCellHeader>Permissões</TableCellHeader>
+                            <TableCellHeader>Telefone</TableCellHeader>
+                            <TableCellHeader>Ações</TableCellHeader>
+                        </TableRow>
+                    </TableHeader>
+                    <tbody>
+                        {employees.map(employee => (
+                            <TableRow key={employee.id}>
+                                <TableCell style={{ paddingLeft: '10px' }}>{employee.name}</TableCell>
+                                <TableCell>{employee.email}</TableCell>
+                                <TableCell>{employee.office}</TableCell>
+                                <TableCell>{employee.permissions}</TableCell>
+                                <TableCell>{employee.phone}</TableCell>
+                                <TableCell>
+                                    <button onClick={() => handleEdit(employee)}><FaEdit /></button>
+                                    <button onClick={() => handleDelete(employee.id)}><FaTrash /></button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </tbody>
+                </Table>
+            </TableContainer>
+        </>
     );
 };
 
